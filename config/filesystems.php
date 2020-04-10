f655fe133287208b6f0f6230867510a4697eb4e5
Asaad Riaz <riaz.asaad@gmail.com>
2020-04-10 05:08:11 -0700

    gcs config

--- config/filesystems.php
+++ config/filesystems.php
@@ -27,6 +27,7 @@ return [
     */
 
     'cloud' => env('FILESYSTEM_CLOUD', 's3'),
+    'cloud' => env('FILESYSTEM_CLOUD', 'gcs'),
 
     /*
     |--------------------------------------------------------------------------
@@ -68,9 +69,9 @@ return [
             'driver' => 'gcs',
             'project_id' => env('GOOGLE_CLOUD_PROJECT_ID'),
             'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET'),
-            'key_file' => env('GOOGLE_CLOUD_KEY_FILE', null), // optional: /path/to/service-account.json
-            #'path_prefix' => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', null), // optional: /default/path/to/apply/in/bucket
-            #'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_API_URI', null), // see: Public URLs below
+            'key_file' => env('GOOGLE_CLOUD_KEY_FILE', base_path()."/gcs-key.json"), // optional: /path/to/service-account.json
+            'path_prefix' => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', null), // optional: /default/path/to/apply/in/bucket
+            'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_API_URI', null), // see: Public URLs below
             #'visibility' => 'public', // optional: public|private
         ],
 
